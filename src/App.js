601582d3272c7cf7d56fb1270b88a41dad30b7ec
Jaydon <jaydonpaul2002@gmail.com>
2022-08-13 16:12:38 -0400

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ContactCard from "./ContactCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return (
+        <div>
+           <ContactCard 
+                imgURL="https://tenor.com/view/black-guy-blue-eyes-rubbing-head-gif-24402199"
+                name="Do do do do"
+                phone="458-7351"
+                email="jaytme02@gmail.com"
+           />
+           
+           <ContactCard 
+                imgURL="https://www.nytimes.com/2020/05/26/us/minneapolis-police-man-died.html"
+                name="Quandale Dingle"
+                phone="456-1681"
+                email="jaydonpaul2002@gmail.com"
+           />
+
+            <ContactCard 
+                imgURL="https://knowyourmeme.com/memes/beast-boy-guy-holding-up-four-fingers"
+                name="Four"
+                phone="403-8371"
+                email="jaydonp002@gmail.com"
+           />
+
+            <ContactCard 
+                imgURL="https://pitchfork.com/reviews/albums/dababy-baby-on-baby/"
+                name="Dababy"
+                phone="456-6977"
+                email="fudgeoflife@gmail.com"
+           />
+
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/MyInfo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MyInfo(){
+    return (
+      <div>
+        <h1>My name is Jaydon</h1>
+        <p>I am a 20 year old with a keen focus on creating highly functionable and reactive web applications.</p>
+        <ul>
+          <li>I would like to visit Bora Bora</li>
+          <li>I would like to visit the Maldives</li>
+          <li>I would like to visit America</li>
+        </ul>
+      </div>
+    )
+    
+}
+
+export default MyInfo
