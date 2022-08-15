e85c46bc1e0e4032376560c2d60bbc50e9e62fc7
Jaydon <jaydonpaul2002@gmail.com>
2022-08-15 19:05:06 -0400

    React

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import React from "react";
-import ContactCard from "./ContactCard";
 
-function App(){
-    return (
-        <div>
-           <ContactCard 
-                imgURL="https://tenor.com/view/black-guy-blue-eyes-rubbing-head-gif-24402199"
-                name="Do do do do"
-                phone="458-7351"
-                email="jaytme02@gmail.com"
-           />
-           
-           <ContactCard 
-                imgURL="https://www.nytimes.com/2020/05/26/us/minneapolis-police-man-died.html"
-                name="Quandale Dingle"
-                phone="456-1681"
-                email="jaydonpaul2002@gmail.com"
-           />
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+    }
 
-            <ContactCard 
-                imgURL="https://knowyourmeme.com/memes/beast-boy-guy-holding-up-four-fingers"
-                name="Four"
-                phone="403-8371"
-                email="jaydonp002@gmail.com"
-           />
+    handleClick() {
+        console.log("Changed")
+    }
 
-            <ContactCard 
-                imgURL="https://pitchfork.com/reviews/albums/dababy-baby-on-baby/"
-                name="Dababy"
-                phone="456-6977"
-                email="fudgeoflife@gmail.com"
-           />
-
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Changed!</button>
+            </div>
+        )
+    }
 }
 
 export default App
